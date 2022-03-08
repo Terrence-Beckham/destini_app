e6f93b42c94525eaef856692e1dbb5a69fcaf468
TerrenceBeckham <terrencebeckham@icloud.com>
2022-03-08 20:29:07 +0200

    Finished the Story class

--- lib/main.dart
+++ lib/main.dart
@@ -29,8 +29,6 @@ class _StoryPageState extends State<StoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        // ignore: todo
-        //TODO: Step 1 - Add background.png to this Container as a background image.
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('images/background.png'),
