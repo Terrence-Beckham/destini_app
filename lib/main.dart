ef6342357b2b9235da28642fb9d97442a25533b7
TerrenceBeckham <terrencebeckham@icloud.com>
2022-03-08 21:48:26 +0200

    added methods to storyBrain to advance the users choice

--- lib/main.dart
+++ lib/main.dart
@@ -63,6 +63,7 @@ class _StoryPageState extends State<StoryPage> {
                     //Choice 1 made by user.
                     // ignore: todo
                     //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    storyBrain.nextStory(1);
                   },
                   color: Colors.red,
                   child: const Text(
@@ -89,6 +90,7 @@ class _StoryPageState extends State<StoryPage> {
                     //Choice 2 made by user.
                     // ignore: todo
                     //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    storyBrain.nextStory(2);
                   },
                   color: Colors.blue,
                   child: const Text(
